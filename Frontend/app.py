2f87d7351a84f4dcc547fa7409595191ac590542
spectre900 <nayakpratham0@gmail.com>
2021-04-06 23:47:15 +0530

    removing unwanted code

--- Frontend/app.py
+++ Frontend/app.py
@@ -8,23 +8,13 @@ app = Flask(__name__)
 def root():
     return render_template('home_page.html')
 
-@app.route('/home', methods = ['POST', 'GET'])
+@app.route('/home')
 def home():
     return render_template('home_page.html')
-
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 @app.route('/statistics/<disease>')
 def statistics(disease):
     if disease == 'dengue':
-        return render_template('disease_statistics.html', disease_name = disease.upper())
-    elif disease == 'flu':
         return render_template('disease_statistics.html', disease_name = disease.upper())
 
 if __name__ == '__main__':
